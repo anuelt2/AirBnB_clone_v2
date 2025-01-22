b49f6477249dbbad8c321e36f0b76287785133e1
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-01-22 16:02:15 +0000

    Add 0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# Script that sets up web servers for the deployment of web_static
+
+if ! command -v nginx > /dev/null 2>&1
+then
+	sudo apt-get update -y
+	sudo apt-get install -y nginx
+	sudo ufw allow "Nginx HTTP"
+	sudo ufw allow OpenSSH
+	sudo ufw enable
+fi
+
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+
+echo "<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+	Welcome to Nginx Server - ALX
+</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+
+NGINX_CONFIG_FILE="/etc/nginx/sites-available/default"
+
+if ! grep -qF "location /hbnb_static/ {" "$NGINX_CONFIG_FILE"
+then
+	sudo cp --backup=numbered "$NGINX_CONFIG_FILE" "$NGINX_CONFIG_FILE".bak
+	sudo sed -i "/server_name/ a\\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" "$NGINX_CONFIG_FILE"
+fi
+
+sudo nginx -t
+exit_code=$?
+
+if (( exit_code == 0 ))
+then
+	sudo service nginx restart
+fi
