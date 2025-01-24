d299717750ca1a42c1f17e8cc490ba725c057f93
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-01-24 20:35:37 +0000

    Add do_deploy to 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to selected
-web servers with function do_deploy
+Fabric script that generates a .tgz archive from contents of web_static
+directory and distributes it to selected web servers
 """
 from fabric.api import *
 import os
+from datetime import datetime
 
 
 web_01_IP = "54.84.143.116"
@@ -13,6 +14,22 @@ web_02_IP = "54.237.22.10"
 env.user = "ubuntu"
 env.key_filename = "~/.ssh/id_rsa"
 env.hosts = [web_01_IP, web_02_IP]
+
+def do_pack():
+    """Function that generates .tgz archive"""
+    local("mkdir -p versions")
+
+    current_time = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+
+    archive_name = f"web_static_{current_time}.tgz"
+    archive_path = f"versions/{archive_name}"
+
+    tar_command = f"tar -czvf {archive_path} web_static"
+    archive = local(tar_command, capture=True)
+
+    if archive.failed:
+        return None
+    return archive_path
 
 
 def do_deploy(archive_path):
