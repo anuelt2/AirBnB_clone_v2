b418fed2b978b45b6e24fc112e4412d77c405e30
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-01-25 05:04:30 +0000

    Add 101-setup_web_static.pp

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,94 @@
+#  Sets up web servers for deployment of web_static
+
+$html_content="
+<html>
+	<head>
+	</head>
+	<body>
+		Welcome to Nginx Server - ALX
+	</body>
+</html>
+"
+
+$location_block="
+	location /hbnb_static {
+		alias /data/web_static/current/;
+	}
+"
+
+$nginx_config_file='/etc/nginx/sites-available/default'
+
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update -y',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['apt-get update'],
+}
+
+file { '/data':
+  ensure  => directory,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0755',
+  recurse => true,
+}
+
+file { '/data/web_static':
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0755',
+}
+
+file { '/data/web_static/releases':
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0755',
+}
+
+file { '/data/web_static/shared':
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0755',
+}
+
+file { '/data/web_static/releases/test':
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0755',
+}
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => file,
+  content => $html_content,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0644',
+}
+
+file { '/data/web_static/current':
+  ensure  => link,
+  target  => '/data/web_static/releases/test',
+  require => File['/data/web_static/releases/test/index.html']
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+}
+
+exec { 'update_nginx_config':
+  command => "sudo sed -i '/server_name _;/ a\ ${location_block}' '${nginx_config_file}'",
+  path    => '/usr/bin:/usr/sbin:/bin',
+  unless  => "grep -qF 'location /hbnb_static/ {' '${nginx_config_file}'",
+  require => File['/data/web_static/current'],
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Exec['update_nginx_config'],
+}
