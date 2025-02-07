741d386e3cd76560e31869da4e68ca56ffe6b7ec
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-02-07 00:16:26 +0000

    Add 2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+"""Script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello():
+    """Handle requests to root URL ("/")"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """Handle requests to "/hbnb" URL"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c(text):
+    """Handle requests to "/c" URL"""
+    format_text = text.replace("_", " ")
+    return f"C {format_text}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
