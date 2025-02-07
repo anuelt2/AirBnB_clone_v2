3eb2d248453d9d48649f4c7f87be287403e83310
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-02-07 11:13:16 +0000

    Add 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web appplication"""
+"""Script that starts a Flask web application"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
