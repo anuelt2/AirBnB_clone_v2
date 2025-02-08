b6d109ff5875130a950ba1a980587068af5f3c26
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-02-08 08:50:32 +0000

    Add close() method to DBStorage class in models/engine/db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -67,3 +67,7 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(Session)
+
+    def close(self):
+        """Closes database connections after each request"""
+        self.__session.remove()
